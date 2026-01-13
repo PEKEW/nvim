7279ed9c6fd1235ff714fc86ddd9dd89a1093385
PEKEW <pekewang@hotmail.com>
2026-01-13 14:49:04 +0800

    latex debug markdown

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 自动换行设置
+vim.opt.wrap = true -- 启用自动换行（视觉上的换行，不改变文件内容）
+vim.opt.linebreak = true -- 在单词边界换行，避免单词被截断
+vim.opt.breakindent = true -- 换行后保持缩进
---
+++ lua/plugins/debug.lua
@@ -0,0 +1,12 @@
+-- 调试相关的自定义配置
+-- extras 已在 lua/config/lazy.lua 中按正确顺序导入
+return {
+  -- 你可以在这里添加额外的调试插件配置或覆盖默认设置
+  -- 例如：
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   opts = function()
+  --     -- 自定义 DAP 配置
+  --   end,
+  -- },
+}
